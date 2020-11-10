a90e55700e0371516bacfb2f70b422f65ae2b761
paruyr123 <paruyr-shahbazyan@yandex.ru>
2020-11-10 12:14:52 +0300

    optimised

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def finished_vodka():
     if productData_links:
         show_group = productData_links[0]
 
-        show_group = [[i for i in vodka if i['link'][0] in show_group]]
+        show_group = [[i for i in vodka if i['link'] in show_group]]
         group_titles = productData_groups[0]
 
     if 'next' in request.form:
@@ -145,13 +145,13 @@ def finished_vodka():
                 ind = productData_groups.index(i)
                 if ind != len(productData_groups)-1:
                     show_group = productData_links[ind + 1]
-                    show_group = [[k for k in vodka if k['link'][0] in show_group]]
+                    show_group = [[k for k in vodka if k['link'] in show_group]]
                     group_titles = productData_groups[ind + 1]
 
                     break
                 else:
                     show_group = productData_links[-1]
-                    show_group = [[k for k in vodka if k['link'][0] in show_group]]
+                    show_group = [[k for k in vodka if k['link'] in show_group]]
                     group_titles = productData_groups[-1]
 
     if 'previous' in request.form:
@@ -161,12 +161,12 @@ def finished_vodka():
                 ind = productData_groups.index(i)
                 if ind != 0:
                     show_group = productData_links[ind - 1]
-                    show_group = [[k for k in vodka if k['link'][0] in show_group]]
+                    show_group = [[k for k in vodka if k['link'] in show_group]]
                     group_titles = productData_groups[ind - 1]
                     break
                 else:
                     show_group = productData_links[0]
-                    show_group = [[k for k in vodka if k['link'][0] in show_group]]
+                    show_group = [[k for k in vodka if k['link'] in show_group]]
                     group_titles = productData_groups[0]
 
     if 'delete_from_finished' in request.form:
@@ -411,11 +411,11 @@ def naming_vodka():
         del_list = read_file('del_list.json')
 
         for i in del_list:
-            del_product = [k for k in vodka if k['link'][0] == i]
+            del_product = [k for k in vodka if k['link'] == i]
             if del_product:
                 del_product[0]['identified'] = False
                 del_product[0].pop('product_id')
-                vodka[vodka.index([n for n in vodka if n['link'][0] == i][0])] = del_product[0]
+                vodka[vodka.index([n for n in vodka if n['link'] == i][0])] = del_product[0]
 
         write_file('vodka.json', vodka)
 
@@ -438,8 +438,8 @@ def naming_vodka():
             vodka = read_file('vodka.json')
 
             products = [i for i in vodka if 'product_id' in i.keys()]
-            product_dict['links'] = [i['link'][0] for i in products if i['product_id'] == product_dict['id']]
-            product_dict['images']['default'] = [i['image'][0] for i in vodka if i['link'][0] in product_dict['links']][0]
+            product_dict['links'] = [i['link'] for i in products if i['product_id'] == product_dict['id']]
+            product_dict['images']['default'] = [i['image'] for i in vodka if i['link'] in product_dict['links']][0]
 
             productData = read_file('productData.json')
 
@@ -829,14 +829,14 @@ def grouping_vodka():
 
     skip_list = read_file('skip_list.json')
 
-    main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+    main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
     if main_drink:
         main_drink = main_drink[0]
     else:
         if skip_list:
 
             del skip_list[0]
-            main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+            main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
             if main_drink:
                 main_drink = main_drink[0]
             write_file('skip_list.json', skip_list)
@@ -873,13 +873,13 @@ def grouping_vodka():
         if not (request.form['skip'] in skip_list):
             skip_list.append(request.form['skip'])
 
-        main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+        main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
         if main_drink:
             main_drink = main_drink[0]
         else:
             if skip_list:
                 del skip_list[0]
-                main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+                main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
                 if main_drink:
                     main_drink = main_drink[0]
 
@@ -887,7 +887,7 @@ def grouping_vodka():
 
     if 'search' in request.form:
         search = request.form['search']
-        result_list = [i for i in vodka if search.lower() in i['name'][0].lower() and not (i is main_drink)]
+        result_list = [i for i in vodka if search.lower() in i['name'].lower() and not (i is main_drink)]
 
     if 'saving' in request.form:
 
@@ -908,7 +908,7 @@ def grouping_vodka():
 
             link_list.remove(match[0])
             if main_drink:
-                link_list.append(main_drink['link'][0])
+                link_list.append(main_drink['link'])
 
             write_file('link_list.json', link_list)
 
@@ -917,7 +917,7 @@ def grouping_vodka():
             link_list = read_file('link_list.json')
 
             for i in link_list:
-                drink = [k for k in vodka if k['link'][0] == i][0]
+                drink = [k for k in vodka if k['link'] == i][0]
                 ind = vodka.index(drink)
                 drink['identified'] = True
                 drink['product_id'] = product_id
@@ -928,15 +928,14 @@ def grouping_vodka():
 
                 write_file('vodka.json', vodka)
 
-            main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+            main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
             if main_drink:
                 main_drink = main_drink[0]
             else:
                 skip_list = read_file('skip_list.json')
                 if skip_list:
                     del skip_list[0]
-                    main_drink = [i for i in vodka if
-                                    i['identified'] is False and not (i['link'][0] in skip_list)]
+                    main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
                     if main_drink:
                         main_drink = main_drink[0]
 
@@ -958,7 +957,7 @@ def grouping_vodka():
 
                 vodka = read_file('vodka.json')
 
-                need_change = [i for i in vodka if i['link'][0] in link_list]
+                need_change = [i for i in vodka if i['link'] in link_list]
                 for i in need_change:
                     ind = vodka.index(i)
                     i['in_productData'] = True
@@ -974,7 +973,7 @@ def grouping_vodka():
 
                 write_file('link_list.json', [])
 
-                link_list.append(main_drink['link'][0])
+                link_list.append(main_drink['link'])
 
                 write_file('link_list.json', link_list)
 
@@ -983,7 +982,7 @@ def grouping_vodka():
                 write_file('link_list.json', [])
 
                 for i in link_list:
-                    drink = [k for k in vodka if k['link'][0] == i][0]
+                    drink = [k for k in vodka if k['link'] == i][0]
                     ind = vodka.index(drink)
                     drink['identified'] = True
                     drink['product_id'] = product_id
@@ -995,7 +994,7 @@ def grouping_vodka():
 
                 write_file('link_list.json', [])
 
-                main_drink = [i for i in vodka if i['identified'] is False and not (i['link'][0] in skip_list)]
+                main_drink = [i for i in vodka if i['identified'] is False and not (i['link'] in skip_list)]
                 if main_drink:
                     main_drink = main_drink[0]
                 else:
